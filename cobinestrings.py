5d0703f07fe77d0e66528e8b3b6e58c7e812a3cd
RUSSONANDAN <51981879+RUSSONANDAN@users.noreply.github.com>
2019-06-20 12:56:04 +0530

    Update cobinestrings.py

--- cobinestrings.py
+++ cobinestrings.py
@@ -4,7 +4,7 @@ def combine():
     username=input('Enter the username')
     password=input('Enter the password')
     #result='servername='+servername+';dbname='+dbname+';username='+username+';password='+password
-    result='servername=%s;dbname=%s;username=%s;password=%s' %(servername,dbname,username,password)
+    result='servername=%s;dbname=%s;username=%s;password=%s;' %(servername,dbname,username,password)
     return(result)
 
 res=combine()
