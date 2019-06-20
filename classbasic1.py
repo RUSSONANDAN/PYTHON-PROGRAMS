563d1a111bdf171ba628568ecb8de6f9091e968b
RUSSONANDAN <51981879+RUSSONANDAN@users.noreply.github.com>
2019-06-20 12:00:45 +0530

    Add files via upload

---
+++ classbasic1.py
@@ -0,0 +1,12 @@
+class Anything:
+    def __init__(self,name):
+        self.name=name
+    def show(self):
+        print('Basic--name:%s' %self.name)
+
+obj1= Anything('Apricot')
+obj1.show()
+
+
+
+        
